7496b680feef967f476fefac0185c749457efb01
RilusMahmud <rilusmahmud1@gmail.com>
2018-06-03 22:44:20 +0600

    Media page working just fine

--- resources/views/admin/media/index.blade.php
+++ resources/views/admin/media/index.blade.php
@@ -22,6 +22,13 @@
                     <td>{{$photo->id}}</td>
                     <td><img height="100px" src="{{$photo->file}}" alt="" class="img-rounded"></td>
                     <td>{{$photo->created_at ? $photo->created_at : 'No date'}}</td>
+                    <td>
+                        {!! Form::open(['method'=>'DELETE', 'action'=>['AdminMediasController@destroy', $photo->id]]) !!}
+                        <div class="form-group">
+                            {!! Form::submit('Delete photo', ['class'=>'btn btn-danger']) !!}
+                        </div>
+                        {!! Form::close() !!}
+                    </td>
                 </tr>
 
             @endforeach
